6f64fffe7a9356e94f90ab9df616e2d4d7837337
Radoslav Ivanov <radoslav4096@gmail.com>
2024-08-31 14:31:00 +0300

    Add solutions for FP 2018

---
+++ Haskell/FinalExamTasks/2018_07.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+main::IO()
+main = do
+  print $ sumMinFix [ (1/), exp, \x -> 2*x - 3 ] [-2, -1, 1, 3] == 2
+
+addDefault val [] = [val]
+addDefault val l = l
+
+sumMinFix fl xl = sum (map (\f -> minimum (addDefault 0 [x | x <- xl, f x == x])) fl)
